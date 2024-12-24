dc4594df2bc6d836ed74efac1de18710a7dbfb9e
Zongiin <jongin0223@gmail.com>
2024-12-24 12:57:10 +0900

    hallfix

--- hall.cpp
+++ hall.cpp
@@ -19,7 +19,7 @@
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
-#define	MODEL_HALL		"data/MODEL/School_rouka_v001.obj"			// 読み込むモデル名
+#define	MODEL_HALL		"data/MODEL/School_rouka_v003.obj"			// 読み込むモデル名
 
 
 
@@ -31,33 +31,36 @@
 //*****************************************************************************
 // グローバル変数
 //*****************************************************************************
-static HALL		g_Hall;						// プレイヤー
+static HALL		g_Hall[MAX_HALL];						// プレイヤー
 
+static DX11_MODEL		g_HallModel;						// プレイヤー
 
-
-
+static BOOL g_Load = FALSE;
 
 //=============================================================================
 // 初期化処理
 //=============================================================================
 HRESULT InitHall(void)
 {
-	g_Hall.load = TRUE;
-	LoadModel(MODEL_HALL, &g_Hall.model);
+	g_Load = TRUE;
+	LoadModel(MODEL_HALL, &g_HallModel);
 
-	g_Hall.pos = XMFLOAT3(0.0f, 0.0f, 0.0f);
-	g_Hall.rot = XMFLOAT3(0.0f, 0.0f, 0.0f);
-	g_Hall.scl = XMFLOAT3(0.02f, 0.02f, 0.02f);
+	
 
-	g_Hall.spd = 0.0f;			// 移動スピードクリア
+	for (int i = 0; i < MAX_HALL; i++)
+	{
+		g_Hall[i].pos = XMFLOAT3(0.0f, 0.0f, 0.0f + (0.02f)*2400*i);
+		g_Hall[i].rot = XMFLOAT3(0.0f, 0.0f, 0.0f);
+		g_Hall[i].scl = XMFLOAT3(0.02f, 0.02f, 0.02f);
 
-	g_Hall.use = TRUE;			// TRUE:生きてる
-	g_Hall.size = 1.0f;	// 当たり判定の大きさ
+		g_Hall[i].spd = 0.0f;			// 移動スピードクリア
 
-	// クォータニオンの初期化
-	XMStoreFloat4(&g_Hall.Quaternion, XMQuaternionIdentity());
+		g_Hall[i].use = TRUE;			// TRUE:生きてる
+		g_Hall[i].size = 1.0f;	// 当たり判定の大きさ
 
-
+		// クォータニオンの初期化
+		XMStoreFloat4(&g_Hall[i].Quaternion, XMQuaternionIdentity());
+	}
 
 	return S_OK;
 }
@@ -68,10 +71,10 @@ HRESULT InitHall(void)
 void UninitHall(void)
 {
 	// モデルの解放処理
-	if (g_Hall.load == TRUE)
+	if (g_Load == TRUE)
 	{
-		UnloadModel(&g_Hall.model);
-		g_Hall.load = FALSE;
+		UnloadModel(&g_HallModel);
+		g_Load = FALSE;
 	}
 
 
@@ -85,7 +88,11 @@ void UpdateHall(void)
 
 #ifdef _DEBUG
 	// デバッグ表示
-	PrintDebugProc("Hall X:%f Y:%f Z:%f\n", g_Hall.pos.x, g_Hall.pos.y, g_Hall.pos.z);
+	for (int i = 0; i < MAX_HALL; i++)
+	{
+		PrintDebugProc("Hall X:%f Y:%f Z:%f\n", g_Hall[i].pos.x, g_Hall[i].pos.y, g_Hall[i].pos.z);
+	}
+	
 #endif
 
 }
@@ -99,40 +106,44 @@ void DrawHall(void)
 
 	// カリング無効
 	//SetCullingMode(CULL_MODE_NONE);
-
-	// ワールドマトリックスの初期化
-	mtxWorld = XMMatrixIdentity();
-
-	// スケールを反映
-	mtxScl = XMMatrixScaling(g_Hall.scl.x, g_Hall.scl.y, g_Hall.scl.z);
-	mtxWorld = XMMatrixMultiply(mtxWorld, mtxScl);
-
-	// 回転を反映
-	mtxRot = XMMatrixRotationRollPitchYaw(g_Hall.rot.x, g_Hall.rot.y + XM_PI, g_Hall.rot.z);
-	mtxWorld = XMMatrixMultiply(mtxWorld, mtxRot);
-
-	// クォータニオンを反映
-	quatMatrix = XMMatrixRotationQuaternion(XMLoadFloat4(&g_Hall.Quaternion));
-	mtxWorld = XMMatrixMultiply(mtxWorld, quatMatrix);
-
-	// 移動を反映
-	mtxTranslate = XMMatrixTranslation(g_Hall.pos.x, g_Hall.pos.y, g_Hall.pos.z);
-	mtxWorld = XMMatrixMultiply(mtxWorld, mtxTranslate);
-
-	// ワールドマトリックスの設定
-	SetWorldMatrix(&mtxWorld);
-
-	XMStoreFloat4x4(&g_Hall.mtxWorld, mtxWorld);
-
-	
-	// 縁取りの設定
-	//SetFuchi(1);
 	SetLightEnable(TRUE);
-	// モデル描画
-	DrawModel(&g_Hall.model);
-	SetLightEnable(FALSE);
-	//SetFuchi(0);
+	
+	for (int i = 0; i < MAX_HALL; i++)
+	{
+		// ワールドマトリックスの初期化
+		mtxWorld = XMMatrixIdentity();
 
+		// スケールを反映
+		mtxScl = XMMatrixScaling(g_Hall[i].scl.x, g_Hall[i].scl.y, g_Hall[i].scl.z);
+		mtxWorld = XMMatrixMultiply(mtxWorld, mtxScl);
+
+		// 回転を反映
+		mtxRot = XMMatrixRotationRollPitchYaw(g_Hall[i].rot.x, g_Hall[i].rot.y, g_Hall[i].rot.z);
+		mtxWorld = XMMatrixMultiply(mtxWorld, mtxRot);
+
+		// クォータニオンを反映
+		quatMatrix = XMMatrixRotationQuaternion(XMLoadFloat4(&g_Hall[i].Quaternion));
+		mtxWorld = XMMatrixMultiply(mtxWorld, quatMatrix);
+
+		// 移動を反映
+		mtxTranslate = XMMatrixTranslation(g_Hall[i].pos.x, g_Hall[i].pos.y, g_Hall[i].pos.z);
+		mtxWorld = XMMatrixMultiply(mtxWorld, mtxTranslate);
+
+		// ワールドマトリックスの設定
+		SetWorldMatrix(&mtxWorld);
+
+		XMStoreFloat4x4(&g_Hall[i].mtxWorld, mtxWorld);
+
+
+		// 縁取りの設定
+		//SetFuchi(1);
+
+		// モデル描画
+		DrawModel(&g_HallModel);
+	}
+	
+	//SetFuchi(0);
+	SetLightEnable(FALSE);
 	// カリング設定を戻す
 	//SetCullingMode(CULL_MODE_BACK);
 }
@@ -143,6 +154,6 @@ void DrawHall(void)
 //=============================================================================
 HALL* GetHall(void)
 {
-	return &g_Hall;
+	return g_Hall;
 }
 
