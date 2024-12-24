cd02c53980270be1bc569060c387f2fdae3fda0c
Zongiin <jongin0223@gmail.com>
2024-12-24 13:04:11 +0900

    change resolution

--- main.h
+++ main.h
@@ -47,8 +47,8 @@ using namespace DirectX;
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
-#define SCREEN_WIDTH	(960)			// ウインドウの幅
-#define SCREEN_HEIGHT	(540)			// ウインドウの高さ
+#define SCREEN_WIDTH	(1280)			// ウインドウの幅
+#define SCREEN_HEIGHT	(720)			// ウインドウの高さ
 #define SCREEN_CENTER_X	(SCREEN_WIDTH / 2)	// ウインドウの中心Ｘ座標
 #define SCREEN_CENTER_Y	(SCREEN_HEIGHT / 2)	// ウインドウの中心Ｙ座標
 
